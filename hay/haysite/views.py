3c38ba92b94085d43fa8d2848217456fdfce0ef9
dogo-satoru <paul.hernandez416@gmail.com>
2022-04-10 01:29:00 -0400

    main.html
    creating of the navbar which will be visible on every page

--- hay/haysite/views.py
+++ hay/haysite/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     context = {}
-    return render(request, 'hay/question.html', context)
+    return render(request, 'hay/main.html', context)
 
 # Create your views here.
